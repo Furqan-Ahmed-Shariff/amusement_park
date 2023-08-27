d1f89d461e3803ac8f5faa12f57989c2ef45b683
Furqan-Ahmed-Shariff <furqan.s1410@gmail.com>
2023-08-27 11:06:47 +0000

    some_views

--- park_app/urls.py
+++ park_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('rides', views.rides, name="rides"),
+    path('stalls', views.stalls, name="stalls"),
+    path('shows', views.shows, name="shows"),
 ]
--- park_app/views.py
+++ park_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "park_app/index.html")
+
+def rides(request):
+    return render(request, "park_app/rides.html", {"rides": Ride.objects.all()})
+
+def stalls(request):
+    return render(request, "park_app/stalls.html", {"stalls": Stall.objects.all()})
+
