aa1911787b2e452d41347bbfeae27ab2d4945624
MilliJack <millijack34@gmail.com>
2023-08-28 09:29:33 +0530

    backend_db_proj_final

--- park_app/views.py
+++ park_app/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from models import *
+from .models import *
+
 
 # Create your views here.
 def index(request):
     return render(request, "park_app/index.html")
 
+
 def rides(request):
     return render(request, "park_app/rides.html", {"rides": Ride.objects.all()})
 
+
 def stalls(request):
     return render(request, "park_app/stalls.html", {"stalls": Stall.objects.all()})
+
 
 def shows(request):
     return render(request, "park_app/shows.html", {"shows": Show.objects.all()})
